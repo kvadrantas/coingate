326b14dab484f751244428a2c73f7327b682bf22
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-10-28 22:55:41 +0300

    Created responsive design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
 import './App.scss';
+import FormXl from './Components/FormXl';
+import FormXs from './Components/FormXs';
+
 
 function App() {
   return (
-    <>
-      veikia
-    </>
+    <div className="main">
+      <div className="bg-block"></div>
+      <div className="content">
+        
+        <div className="section1">
+          <h1>
+            Buy Bitcoin, <span>Ethereum, Litecoin and other crypto</span> online
+          </h1>
+          <FormXs className="FormXs"/>
+          <p>Why bother going through complicated exchanges? Buy cryptocurrency with top payment methods like SEPA bank transfer, Credit and Debit Card, Apple Pay, Mobile balance or Klarna. You can buy Bitcoin, Ethereum or any other popular crypto directly to your personal wallet without making any initial deposits. It's as easy as it gets!</p>
+          <a className="start-now" href="">Start now &gt;</a>
+        </div>
+
+        <div className="section2">
+          <FormXl className="FormXl"/>
+        </div>
+      </div>
+      
+    </div>
   );
 }
 
---
+++ src/Components/FormXl.jsx
@@ -0,0 +1,46 @@
+import sepa from "../img/sepa.jpeg"
+
+
+function FormXl() {
+
+    return (
+            <section className="FormXl">
+                <div className="back-block">
+
+                </div>
+                <div className="front-block">
+                    <div className="pay">
+                        <label htmlFor="">Pay</label>
+                        <input type="text" />
+                        <i className="fas fa-euro-sign"></i>
+                        <select name="" id="">
+                            <option value="">EUR</option>
+                            <option value="">BTC</option>
+                        </select>
+                    </div>
+                    <div className="buy">
+                        <label htmlFor="">Buy</label>
+                        <input type="text" />
+                        <i className="fas fa-euro-sign"></i>
+                        <select name="" id="">
+                            <option value="">EUR</option>
+                            <option value="">BTC</option>
+                        </select>
+                    </div>
+                    <p className="payment-method">Payment method</p>
+                    <div className="payment-method-box">
+                        <img src={sepa} alt="sepa" />
+                        <select name="" id="">
+                            <option value="">Bank transfer</option>
+                            <option value="">Paysera</option>
+                            <option value="">Paypal</option>
+                        </select>
+                    </div>
+                    <button>Buy BTC</button>
+                </div>
+            </section>
+    )
+
+}
+
+export default FormXl;
